9bb74f0d4522cc16b4546812fc7874acf0ffc567
Sylbird <84147332+Sylbird@users.noreply.github.com>
2025-12-31 19:22:11 -0500

    Fiix `Turno` and `Paradero` entities to reflect the database

--- src/entities/paradero.ts
+++ src/entities/paradero.ts
@@ -1,45 +1,52 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
-@Entity('paraderos')
+@Entity("paraderos")
 export class Paradero {
+  @PrimaryColumn({ name: "id_paradero" })
+  idParadero: string;
 
-    @PrimaryColumn({ name: 'id_paradero' })
-    idParadero: string;
+  @Column({ name: "nombre" })
+  nombre: string;
 
-    @Column({ name: 'nombre' })
-    nombre: string;
+  @Column({ name: "direccion", nullable: true })
+  direccion: string;
 
-    @Column({ name: 'direccion', nullable: true })
-    direccion: string;
+  @Column({
+    name: "lat",
+    type: "decimal",
+    precision: 10,
+    scale: 7,
+    nullable: true,
+  })
+  lat: number;
 
-    @Column({ name: 'lat', type: 'decimal', precision: 10, scale: 7, nullable: true })
-    lat: number;
+  @Column({
+    name: "lng",
+    type: "decimal",
+    precision: 10,
+    scale: 7,
+    nullable: true,
+  })
+  lng: number;
 
-    @Column({ name: 'lng', type: 'decimal', precision: 10, scale: 7, nullable: true })
-    lng: number;
+  @Column({ name: "radio_metros", default: 200 })
+  radioMetros: number;
 
-    @Column({ name: 'radio_metros', default: 200 })
-    radioMetros: number;
+  @Column({ name: "estado_auditoria" })
+  estadoAuditoria: number;
 
-    @Column({ name: 'capacidad_motos' })
-    capacidadMotos: number;
+  @CreateDateColumn({ name: "fecha_creacion" })
+  fechaCreacion: Date;
 
-    @Column({ name: 'es_subparadero' })
-    esSubparadero: boolean;
-
-    @Column({ name: 'id_paradero_principal', nullable: true })
-    idParaderoPrincipal: string;
-
-    @Column({ name: 'estado_auditoria' })
-    estadoAuditoria: number;
-    
-    @CreateDateColumn({ name: 'fecha_creacion' })
-    fechaCreacion: Date;
-    
-    @UpdateDateColumn({ name: 'fecha_modificacion' })
-    fechaModificacion: Date;
+  @UpdateDateColumn({ name: "fecha_modificacion" })
+  fechaModificacion: Date;
 }
-
 
 /*-- ==========================
 -- PARADEROS
@@ -51,10 +58,7 @@ CREATE TABLE paraderos (
   direccion VARCHAR(255),
   lat DECIMAL(10,7),
   lng DECIMAL(10,7),
-  radio_metros INT DEFAULT 100,
-  capacidad_motos INT NOT NULL,
-  es_subparadero BOOLEAN DEFAULT FALSE,
-  id_paradero_principal VARCHAR(36) REFERENCES paraderos(id_paradero),
+  radio_metros INT DEFAULT 200,
   estado_auditoria CHAR(1) NOT NULL DEFAULT '1',
   fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
