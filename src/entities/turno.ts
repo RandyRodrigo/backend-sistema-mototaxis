6463be5a70b94849f3c0e318c7ac9107d18d99c6
RandyRodrigo <jrf_jose@hotmail.com>
2026-01-10 21:23:30 -0500

    Add new fields to Turno entity and update schema
    Added 'idParadero', 'cantidadMotos', and 'diasSemana' fields to the Turno entity. Updated the corresponding SQL table definition to include these fields and a foreign key constraint for 'id_paradero'.

--- src/entities/turno.ts
+++ src/entities/turno.ts
@@ -1,33 +1,37 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryColumn,
-  UpdateDateColumn,
-} from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 
-@Entity("turnos")
+@Entity('turnos')
 export class Turno {
-  @PrimaryColumn({ name: "id_turno" })
-  idTurno: string;
 
-  @Column({ name: "nombre", length: 50 })
-  nombre: string;
+    @PrimaryColumn({ name: 'id_turno' })
+    idTurno: string;
 
-  @Column({ name: "hora_inicio", type: "time" })
-  horaInicio: string;
+    @Column({ name: 'id_paradero' })
+    idParadero: string;
 
-  @Column({ name: "hora_fin", type: "time" })
-  horaFin: string;
+    @Column({ name: 'nombre', length: 50 })
+    nombre: string;
 
-  @Column({ name: "estado_auditoria" })
-  estadoAuditoria: number;
+    @Column({ name: 'hora_inicio', type: 'time' })
+    horaInicio: string;
 
-  @CreateDateColumn({ name: "fecha_creacion" })
-  fechaCreacion: Date;
+    @Column({ name: 'hora_fin', type: 'time' })
+    horaFin: string;
 
-  @UpdateDateColumn({ name: "fecha_modificacion" })
-  fechaModificacion: Date;
+    @Column({ name: 'cantidad_motos' })
+    cantidadMotos: number;
+
+    @Column({ name: 'dias_semana' })
+    diasSemana: string;
+
+    @Column({ name: 'estado_auditoria' })
+    estadoAuditoria: number;
+    
+    @CreateDateColumn({ name: 'fecha_creacion' })
+    fechaCreacion: Date;
+    
+    @UpdateDateColumn({ name: 'fecha_modificacion' })
+    fechaModificacion: Date;
 }
 
 /*-- ==========================
@@ -35,11 +39,15 @@ export class Turno {
 -- ==========================
 DROP TABLE IF EXISTS turnos;
 CREATE TABLE turnos (
-  id_turno VARCHAR(36) PRIMARY KEY,
-  nombre VARCHAR(100) NOT NULL,
-  hora_inicio TIME NOT NULL,
-  hora_fin TIME NOT NULL,
-  estado_auditoria CHAR(1) NOT NULL DEFAULT '1',
-  fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    id_turno VARCHAR(36) PRIMARY KEY,
+    id_paradero VARCHAR(36) NOT NULL,
+    nombre VARCHAR(50),
+    hora_inicio TIME NOT NULL,
+    hora_fin TIME NOT NULL,
+    cantidad_motos INT NOT NULL,
+    dias_semana SET('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'),
+    estado_auditoria CHAR(1) NOT NULL DEFAULT '1',
+	fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_paradero) REFERENCES paraderos(id_paradero)
 ); */
