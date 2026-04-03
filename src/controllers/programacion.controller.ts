4d282f3a7b36ed6c056aceb4a33d08c7e0696b2d
Sylbird <84147332+Sylbird@users.noreply.github.com>
2026-04-03 00:54:11 -0500

    Comment out unimplemented update and delete endpoints (TEMP)

--- src/controllers/programacion.controller.ts
+++ src/controllers/programacion.controller.ts
@@ -28,38 +28,45 @@ export const insertarProgramacion = async (req: Request, res: Response) => {
         res.status(500).json(BaseResponse.error(error.message));
     }
 }
+// TODO: REVISAR PORQUE ESTO NO EXISTE
+// export const actualizarProgramacion = async (req: Request, res: Response) => {
+//   try {
+//     const { error } = actualizarProgramacionSchema.validate(req.body);
+//     if (error) {
+//         res.status(400).json(BaseResponse.error(error.message, 400));
+//         return;
+//     }
+//     const { idProgramacion } = req.body;
+//     const programacionActualizada = await programacionService.actualizarProgramacion(idProgramacion, req.body);
+//     res.status(200).json(BaseResponse.success(programacionActualizada, 'Programacion actualizada correctamente'));
+//   } catch {
+
+//   }
+// }
 
 export const actualizarProgramacion = async (req: Request, res: Response) => {
-  try {
-    const { error } = actualizarProgramacionSchema.validate(req.body);
-    if (error) {
-        res.status(400).json(BaseResponse.error(error.message, 400));
-        return;
-    }
-    const { idProgramacion } = req.body;
-    const programacionActualizada = await programacionService.actualizarProgramacion(idProgramacion, req.body);
-    res.status(200).json(BaseResponse.success(programacionActualizada, 'Programacion actualizada correctamente'));
-  } catch {
-
-  }
 }
 
+// TODO: REVISAR PORQUE ESTO NO EXISTE
+// export const eliminarProgramacion = async (req: Request, res: Response) => {
+//     try {
+//         const { idProgramacion } = req.params;
+//         const programacion = await programacionService.obtenerProgramacionPorId(Number(idProgramacion));
+
+//         if (!programacion) {
+//             res.status(404).json(BaseResponse.error('Programacion no encontrada', 404));
+//             return;
+//         }
+
+//         await programacionService.eliminarProgramacion(Number(idProgramacion));
+//         res.status(200).json(BaseResponse.success(null, 'Programacion eliminada correctamente'));
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json(BaseResponse.error(error.message));
+//     }
+// }
+
 export const eliminarProgramacion = async (req: Request, res: Response) => {
-    try {
-        const { idProgramacion } = req.params;
-        const programacion = await programacionService.obtenerProgramacionPorId(Number(idProgramacion));
-
-        if (!programacion) {
-            res.status(404).json(BaseResponse.error('Programacion no encontrada', 404));
-            return;
-        }
-
-        await programacionService.eliminarProgramacion(Number(idProgramacion));
-        res.status(200).json(BaseResponse.success(null, 'Programacion eliminada correctamente'));
-    } catch (error) {
-        console.error(error);
-        res.status(500).json(BaseResponse.error(error.message));
-    }
 }
 
 export const obtenerProgramacionDeHoyPorMoto = async (req: Request, res: Response) => {
